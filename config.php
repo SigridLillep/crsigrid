e79881bcd885f26d348dbfdab7149cc8f089a17b
SigridLillep <sigridlillep@gmail.com>
2026-03-12 12:27:46 +0200

    järgmine kord kuvame

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_server = 'localhost';
+$db_andmebaas = 'crsigrid';
+$db_kasutaja = 'Sigrid';
+$db_salasona = 'Passw0rd';
+
+// ühendus andmebaasiga
+$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+
+// ühenduse kontroll
+if (!$yhendus) {
+    die('Ei saa ühendust andmebaasiga');
+}
+?>
