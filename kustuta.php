3b41bb0d9fbe458781d74721b0716efceae8c289
SigridLillep <sigridlillep@gmail.com>
2026-04-16 12:12:52 +0300

    kustuta

---
+++ kustuta.php
@@ -0,0 +1,9 @@
+<?php
+
+include("config.php"); 
+$paring = "DELETE FROM cars WHERE ID = ".$_GET['id']."";
+$valjund = mysqli_query($yhendus, $paring);
+if($valjund){
+    header("Location:admin.php");
+}
+?>
