c5e65d8bb434ba4df3a9f838d793eb3b77987a48
SigridLillep <sigridlillep@gmail.com>
2026-04-16 11:50:37 +0300

    admin

--- admin.php
+++ admin.php
@@ -1,12 +1,41 @@
 <h1>Admin</h1>
 
+<table border="1">
+    <tr>
+        <td>Mark</td>
+        <td>Model</td>
+        <td>Engin</td>
+        <td>Fuel</td>
+        <td>Price</td>
+        <td>Image</td>
+        <td>Kustuta</td>
+        <td>Muuda</td>
+    </tr>
+
+
 <?php
 
 include("config.php"); 
 
+$paring = "SELECT * FROM cars LIMIT 8";
+$valjund = mysqli_query($yhendus, $paring); // mysql käsu saatmine andmebaasile
+
+while($rida = mysqli_fetch_assoc($valjund)){
+    //var_dump($rida);
+    echo "<tr>
+    <td>".$rida['mark']."</td>
+    <td>".$rida['model']."</td>
+    <td>".$rida['engin']."</td>
+    <td>".$rida['fuel']."</td>
+    <td>".$rida['price']."</td>
+    <td>".$rida['image']."</td>
+    <td><a href='kustuta.php'>Kustuta</a></td>
+    <td><a href='muuda.php'>Muuda</a></td>
 
 
-
-
+    </tr>";
+}
 
 ?>
+
+</table>
--- readme.md
+++ readme.md
@@ -6,3 +6,5 @@ Kui kooliarvutist on su andmebaasikaust kadunud C:\xampp\htdocs\crsigrid
 siis klooni githubist sinna uuesti + xamppis pane apache ja mysql tööle, mysql adminis lähed andmebaasi peale, lood uuesti sinna crsigrid, siis sinna sql alla kopeerid kogu crsigrid.sql sisu + kasutajate alla lood täpselt sama kasutaja, mis config.php all on.
 
 http://localhost/crsigrid/
+
+Kui xamppis mysql ei lähe tööle (annab errorit), siis C:\xampp\mysql siin kaustas tee backup ja data kaustast koopia, siis kustuta algne data kaust täiesti ära ja muuda backup-koopia nimi data'ks.
