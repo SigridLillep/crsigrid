6673f0cbbd1aec22e96d0eb33837a7be9e8f6eaa
SigridLillep <sigridlillep@gmail.com>
2026-04-17 11:47:14 +0300

    muuda

--- muuda.php
+++ muuda.php
@@ -1,27 +1,40 @@
 <?php
     include("config.php");
 
-    // if (!empty($_GET)) {
-    //     $mark = $_GET['mark'];
-    //     $model = $_GET['model'];
-    //     $engin = $_GET['engin'];
-    //     $fuel = $_GET['fuel'];
-    //     $price = $_GET['price'];
-    //     $image = $_GET['image'];
-
-        // $paring = "INSERT INTO cars (mark, model, engin, fuel, price, image) VALUES ('".$mark."', '".$model."', '".$engin."', '".$fuel."', '".$price."', '".$image."')";
+    if (!empty($_GET['muuda_id'])) {
+         $id = $_GET['muuda_id'];
+        $mark = $_GET['mark'];
+        $model = $_GET['model'];
+        $engin = $_GET['engin'];
+        $fuel = $_GET['fuel'];
+        $price = $_GET['price'];
+        $image = $_GET['image'];
         
+        $paring = "UPDATE cars 
+        SET mark = '".$mark."', 
+        model = '".$model."', 
+        engin = '".$engin."', 
+        fuel = '".$fuel."', 
+        price = '".$price."', 
+        image = '".$image."' 
+        WHERE cars.id = ".$id."";
+
+   
+        $valjund = mysqli_query($yhendus, $paring);
+        $tulemus = mysqli_affected_rows($yhendus);
+            if ($tulemus == 1) {
+                header("Location: admin.php");
+            }
+        }
+
         $paring = "SELECT * FROM cars WHERE id=".$_GET['id']."";
         $valjund = mysqli_query($yhendus, $paring);
         $rida = mysqli_fetch_assoc($valjund);
-       // $tulemus = mysqli_affected_rows($yhendus);
-    //     if ($tulemus == 1) {
-    //         header("Location: admin.php");
-    //     }
-    // }
-?>
 
+?>
+       
 <form action="muuda.php" method="get">
+    <input type="hidden" name="muuda_id" value="<?php echo $rida['id']; ?>"><br>
     Mark <input type="text" name="mark" value="<?php echo $rida['mark']; ?>"><br>
     Model <input type="text" name="model" value="<?php echo $rida['model']; ?>"><br>
     Engin <input type="text" name="engin" value="<?php echo $rida['engin']; ?>"><br>
@@ -29,5 +42,4 @@
     Price <input type="number" name="price" value="<?php echo $rida['price']; ?>"><br>
     Image <input type="text" name="image" value="<?php echo $rida['image']; ?>"><br>
     <input type="submit" value="Muuda"><br>
-
-</form>
+</form> 
